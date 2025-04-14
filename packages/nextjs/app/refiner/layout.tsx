00ad3855b6771632dc44d54a8a0b6cd3ed01550a
Aimable01 <aimable.kwizera14@gmail.com>
2025-04-14 11:56:58 +0200

    feat: refiner portal

---
+++ packages/nextjs/app/refiner/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import Sidebar from "~~/components/dashboard/Sidebar";
+import TopBar from "~~/components/dashboard/topBar";
+import { getSidebarItems } from "~~/types/dashboard/sidebarItems";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const sidebarItems = getSidebarItems("/miner");
+
+export default function MinerLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={`${inter.variable} font-sans bg-lightBlack flex text-white h-screen`}>
+      <Sidebar basePath="/refiner" />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <TopBar sidebarItems={sidebarItems} basePath="/miner" />
+        <main className="flex-1 overflow-y-auto px-6 py-4">{children}</main>
+      </div>
+    </div>
+  );
+}
