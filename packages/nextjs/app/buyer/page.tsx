a0f0b1d5a2020cc81ecb99b5c88ee03a976f401e
0xJonaseb11 <sebejaz99@gmail.com>
2025-05-09 10:37:52 +0200

    update: stash changes to resolve nextjs server error

--- packages/nextjs/app/buyer/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page() {
-  redirect("/buyer/overview");
-  return null;
-}
--- packages/nextjs/utils/dashboard/cn.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line prettier/prettier
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
