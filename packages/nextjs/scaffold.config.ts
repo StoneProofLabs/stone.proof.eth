9be6f9bf7861faaa2f0ca757fc605cab3d0d68c9
0xJonaseb11 <sebejaz99@gmail.com>
2025-05-16 21:14:47 +0200

    update: scaffold config and hardhat

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -25,23 +25,23 @@ export type ScaffoldConfig = {
       abi: any;
     };
     DisputeResolution: {
-      adddress: string;
+      address: string;
       abi: any;
     };
     LogisticsManager: {
-      adddress: string;
+      address: string;
       abi: any;
     };
     TransactionLog: {
-      adddress: string;
+      address: string;
       abi: any;
     };
     MineralRegistry: {
-      adddress: string;
+      address: string;
       abi: any;
     };
     PrivacyGuard: {
-      adddress: string;
+      address: string;
       abi: any;
     };
     Tokenization: {
@@ -49,7 +49,7 @@ export type ScaffoldConfig = {
       abi: any;
     };
     SupplychainValidator: {
-      adddress: string;
+      address: string;
       abi: any;
     };
   };
@@ -104,13 +104,19 @@ const scaffoldConfig = {
 
   contracts: {
     RolesManager: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      address: "0xcF8e0CeFeeCa887af0d1421896FB8bAF0697101b",
       abi: RolesManager.abi,
     },
     MineralWarehouse: {
-      address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
+      address: "0x025E35cB1AFE15BA3890fbd08D22195fAf18a1b1",
       abi: MineralWarehouse.abi,
     },
+    DisputeResolution: {
+      address: "0x5d46F24109fF5E0f8ef50913Fc673b8CAf30587d",
+      abi: DisputeResolution.abi,
+    },
+
+    // BASE MAINNET DEPLOYED STOPS HERE
     LogisticsManager: {
       address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
       abi: LogisticsManager.abi,
@@ -119,17 +125,13 @@ const scaffoldConfig = {
       address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
       abi: MineralRegistry.abi,
     },
-    SuplychainValidator: {
+    SupplychainValidator: {
       address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
       abi: SupplychainValidator.abi,
     },
     TransactionLog: {
       address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
       abi: TransactionLog.abi,
-    },
-    DisputeResolution: {
-      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
-      abi: DisputeResolution.abi,
     },
     PrivacyGuard: {
       address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
